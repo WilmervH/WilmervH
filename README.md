ce6ff55a72db19a1d7e7815493855b7c2995ed61
WilmervH <83898838+WilmervH@users.noreply.github.com>
2021-11-12 22:25:01 +0200

    updated readme, added code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-- 👋 Hello, I’m @WilmervH and welcome to my profile.
-- I´m new to GitHub.
-- bye!
+#### WilmervH
+# Top three **mysteries** of the world
+
+2. What is the meaning of life
+3.  What is time and does it even exist
+:exclamation: Exception in thread "main"
+java.lang.ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
+	at Main.main(Main.java:14)
---
+++ readmeGenerator.java
@@ -0,0 +1,17 @@
+//code that generated README.md
+//
+//Made by WilmervH with java and love
+
+public class Main
+{
+    public static void main(String[] args) {
+        String[] mysteries = {"1. Will my credit card go through", "2. What is the meaning of life", "3. What is time and does it even exist"};
+        
+        //output
+        System.out.println("#### WilmervH \n# Top three **mysteries** of the world");
+
+        for (int i = 1; i <= mysteries.length; i++) {
+            System.out.println(mysteries[i]);
+        }
+    }
+}
